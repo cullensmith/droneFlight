e1582329eb7cfaf14675b3515aa3ba39d644f0d4
Cullen <smith.cmaps@gmail.com>
2023-02-20 21:43:57 -0500

    included full notebook for v1

--- src/main.py
+++ src/main.py
@@ -1,13 +1,22 @@
 import geopandas as gpd
 
-def main():
-    hazard = gpd.read_file('data/sampleHazard.geojson')
+
+# convert the notebook
+# add in the weather api calls
+
+def import_data(file):
+    hazard = gpd.read_file(file)
     print(f"warning: {hazard['name'][0]}")
     hazard.plot()
 
 def check_projection():
     #check the projections of the incoming data
     pass
+
+def main():
+    flight_path = import_data('data/samplePath.gpkg')
+    hazard = import_data('data/sampleHazard.geojson')
+    
 
 if __name__ == '__main__':
     print('importing the hazard...')
